d4233180c0d46f92e7e8ee12a1ed6a5881ce9747
Julian Meisel <julian.meisel@dwpbank.de>
2020-09-22 14:47:48 +0200

    Specify username

--- keyboards/preonic/keymaps/workman-neoscaler/rules.mk
+++ keyboards/preonic/keymaps/workman-neoscaler/rules.mk
@@ -1,3 +1,5 @@
+USER_NAME := neoscaler
+
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = yes          # Audio output on port C6
